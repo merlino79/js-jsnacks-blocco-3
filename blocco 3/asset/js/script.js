f825c7772b6a91b9c99ffb0786960c6b296b050f
Merlino79 <alessandro.ciancio@hotmail.com>
2021-04-27 12:51:17 +0200

    fermo

--- blocco 3/asset/js/script.js
+++ blocco 3/asset/js/script.js
@@ -2,16 +2,18 @@ $(function() {
 
     var arrBidimensionale = [
         ['a', 'b', 'c', ],
-        [1, 2, 3, 4, ],
+        [1, 2, 3, 4, 5, ],
 
     ];
     console.log(arrBidimensionale);
 
+
+
     for (arr of arrBidimensionale) {
         for (var arrIn of arr) {
-            console.log(arrIn);
+            console.log(arr + arrIn);
         }
-        //console.log('--------->')
+        console.log('--------->')
     }
 
 
