a7ac877301f68a465e9b6a259abc6c69a8fda57b
Merlino79 <alessandro.ciancio@hotmail.com>
2021-05-03 12:28:54 +0200

    work

--- blocco 4/asset/js/script.js
+++ blocco 4/asset/js/script.js
@@ -1,5 +1,61 @@
-$(function(){
+// Crea un array composto da 10 oggetti che rappresentano un’automobile.
+// Ogni oggetto automobile avrà le seguenti proprietà: marca, modello, alimentazione (benzina, diesel, gpl, elettrico, metano).
+// Dividi le automobili in 3 array separati: inserisci nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+// Infine stampa separatamente i 3 array.
 
-	
+
+
+
+
+$(function() {
+
+    const automobili = [
+
+        {
+            nome: 'abart',
+
+        },
+        {
+            nome: 'lancia',
+
+        },
+        {
+            nome: 'hyunday',
+
+        },
+        {
+            nome: 'honda',
+
+        },
+        {
+            nome: 'ferrari',
+
+        },
+        {
+            nome: 'citroen',
+
+        },
+        {
+            nome: 'cupra',
+
+        },
+        {
+            nome: 'bmw',
+
+        },
+        {
+            nome: 'audi',
+
+        },
+        {
+            nome: 'alfa romeo',
+
+        },
+    ];
+    console.log(automobili);
+
+
+
+
 
 });
